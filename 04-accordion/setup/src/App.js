d874a3ce9f86cbc8fd8c3a3990b9b0090377eb8a
David Encinas <Davidaencinas@gmail.com>
2023-01-20 16:45:38 -0500

    04-accordion-completed

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
+
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {data.map(item => (<SingleQuestion key={item.id} data={item} /> ))}
+        </section>
+      </div>
+    </main>
+    )
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = (props) => {
+  const { id, title, info } = props.data
+
+  const [ showInfo, setShowInfo ] = useState(false);  
+
+  return (
+    <article className="question" id={`${id}_title`} key={`${id}_title`}>
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>      
+      {showInfo && <p>{info}</p>}
+    </article>
+
+    
+    
+  );
 };
 
 export default Question;
