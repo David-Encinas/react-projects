8ecd12f7f2a8bffe2c7b0a5b716dfde7c6ac26f8
David Encinas <Davidaencinas@gmail.com>
2023-02-20 11:53:20 -0500

    13-stripe-submenu/completed

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import { useGlobalContext } from './context'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {closeSubMenu} = useGlobalContext();
+  
+
+  return (
+    <section className="hero" onMouseOver={closeSubMenu}>
+      <div className="hero-center">
+        <article className='hero-info'>
+          <h1>Payments infrastructure for the internet</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum blanditiis rem, illo sapiente nostrum non corporis aliquid voluptatibus architecto accusamus!</p>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className='phone-img' alt='phone'/>
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -1,9 +1,44 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
+import sublinks from './data';
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  const  {openSideBar, openSubMenu, closeSubMenu } = useGlobalContext();
+
+  const displaySubmenu = (e) => {
+    const page = e.target.id;    
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    openSubMenu(page, { center, bottom })
+  }  
+
+  const handleSubMenu = (e) => {
+    if (!e.target.classList.contains('link-btn')) { closeSubMenu() };
+  }
+  
+  return (
+    <nav className="nav" onMouseOver={handleSubMenu}>
+      <div className="nav-center">
+        <div className="nav-header">
+            <img src={logo} alt="logo stripe" className='nav-logo' />
+            <button className="btn toggle-btn" onClick={openSideBar}>
+              <FaBars />
+            </button>
+        </div>
+        <ul className="nav-links">
+          {sublinks.map( (item, index) => { 
+            return <li key={index}>
+              <button className="link-btn" id={item.page} onMouseOver={displaySubmenu}>{item.page}</button>
+            </li>
+          })}          
+        </ul>
+        <button className="btn signin-btn">sign in</button>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
