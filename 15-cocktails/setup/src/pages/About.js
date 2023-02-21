35c0e0013feea9f1d9c42e58ccc79cfc08bb08fc
David Encinas <Davidaencinas@gmail.com>
2023-02-20 22:34:46 -0500

    15-cocktails/complete

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus nihil fugiat dolorem reiciendis dolores culpa consequuntur suscipit non placeat, dolore voluptatum a ipsa illum, ducimus quibusdam ipsam eligendi. Quis optio minima quia sint aperiam? Eveniet commodi vitae alias magnam minus?</p>
+    </section>
   )
 }
 
